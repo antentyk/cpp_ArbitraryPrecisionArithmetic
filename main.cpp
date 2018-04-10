9ca81b0d10b27568f990e47925b711b5cd8c3651
george <antentyk@ucu.edu.ua>
2018-04-11 00:11:24 +0300

    minor fixes

--- main.cpp
+++ main.cpp
@@ -10,26 +10,4 @@ using std::endl;
 using std::string;
 
 int main(){
-    string s1, s2, s3;
-
-    while(cin >> s1 >> s2 >> s3){
-        MBigInt
-            a(s1),
-            b(s2),
-            c(s3);
-        
-        if(a / b != c){
-            cout << "Error" << endl;
-            cout << a << endl;
-            cout << b << endl;
-
-            cout << "Should be " << c <<endl;
-            cout << "You got   " << (a / b) << endl;
-            return 0;
-        }
-    }
-
-    cout << "Everyting went ok" << endl;
-
-    return 0;
 }
